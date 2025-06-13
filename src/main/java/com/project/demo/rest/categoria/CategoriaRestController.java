db74066c151a552d7a3dd408a8a03e59aff041da
Carlos Eduardo Quesada Ramirez <carloseduardoquesadaramirez@gmail.com>
2025-06-12 20:39:42 -0600

    Creación "RestController" de "Categoría"

--- src/main/java/com/project/demo/rest/categoria/CategoriaRestController.java
+++ src/main/java/com/project/demo/rest/categoria/CategoriaRestController.java
@@ -1,4 +1,50 @@
 package com.project.demo.rest.categoria;
 
+import com.project.demo.logic.entity.categoria.Categoria;
+import com.project.demo.logic.entity.categoria.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaRestController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'USER')")
+    public List<Categoria> findAllCategories() {
+        return categoriaRepository.findAll();
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public Categoria updateCategoria(@PathVariable Long id, @RequestBody Categoria categoria) {
+        return categoriaRepository.findById(id)
+                .map(existingCategorie -> {
+                    existingCategorie.setNombre(categoria.getNombre());
+                    existingCategorie.setDescripcion(categoria.getDescripcion());
+                    return categoriaRepository.save(existingCategorie);
+                })
+                .orElseGet(() -> {
+                    categoria.setId(id);
+                    return categoriaRepository.save(categoria);
+                });
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public Categoria addCategoria(@RequestBody Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @DeleteMapping("/{id}")
+    public void deleteCategoria(@PathVariable Long id) {
+        categoriaRepository.deleteById(id);
+    }
 }
