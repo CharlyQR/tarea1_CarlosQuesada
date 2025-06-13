cc7a58aa98a52417d06d43165f3d0dc328cdf95c
Carlos Eduardo Quesada Ramirez <carloseduardoquesadaramirez@gmail.com>
2025-06-12 22:53:22 -0600

    Verificando error de id en Productos que no se rienicia

--- src/main/java/com/project/demo/logic/entity/producto/Producto.java
+++ src/main/java/com/project/demo/logic/entity/producto/Producto.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+    private Long id;
 
     private String nombre;
     private String descripcion;
